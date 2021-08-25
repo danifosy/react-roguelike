f5e6f4c4e0d9a5b36df6c628c47ae469ff6326a1
danifosy <mail@daniha.de>
2021-08-25 20:57:20 +0200

    Add types to Player.tsx

--- src/app/Player.tsx
+++ src/app/Player.tsx
@@ -1,17 +1,20 @@
 class Player {
+  x: number;
+  y: number;
+  size: number;
+
   constructor(x: number, y: number, size: number) {
     this.x = x;
     this.y = y;
     this.size = size;
   }
 
-  move(dx: number, dy: number) {
+  move(dx: number, dy: number): void {
     this.x += dx;
     this.y += dy;
   }
 
-  draw(context) {
-    context.fillStyle = '#f00';
+  draw(context: CanvasRenderingContext2D): void {
     context.textBaseline = 'hanging';
     context.font = '16px Helvetica';
     context.fillText('🦊', this.x * this.size, this.y * this.size);
