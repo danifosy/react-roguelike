b513258eb763b685a709cc61dd83ce43e05ea63d
danifosy <mail@daniha.de>
2021-08-25 21:33:55 +0200

    Add and export types to InputManager.tsx

--- src/app/ReactRogue/InputManager.tsx
+++ src/app/ReactRogue/InputManager.tsx
@@ -1,17 +1,25 @@
-class InputManager {
-  observer = [];
+export type InputHandlerAction = string;
+export type InputHandlerData = { x: number; y: number };
+export type InputHandlerFunction = (
+  action: InputHandlerAction,
+  data: InputHandlerData
+) => void;
 
-  subscribe(fn) {
+export class InputManager {
+  observer: InputHandlerFunction[] = [];
+
+  subscribe(fn: InputHandlerFunction): void {
     this.observer.push(fn);
   }
-  unsubscribe(fn) {
+  unsubscribe(fn: InputHandlerFunction): void {
     this.observer = this.observer.filter((subscriber) => subscriber !== fn);
   }
 
-  broadcast(action, data) {
+  broadcast(action: InputHandlerAction, data: InputHandlerData): void {
     this.observer.forEach((subscriber) => subscriber(action, data));
   }
-  handleKeys = (e) => {
+
+  handleKeys(e: KeyboardEvent): void {
     e.preventDefault();
     switch (e.keyCode) {
       case 37:
@@ -29,14 +37,12 @@ class InputManager {
       default:
         break;
     }
-  };
-
-  bindKeys() {
-    document.addEventListener('keydown', this.handleKeys);
   }
-  unbindKeys() {
-    document.removeEventListener('keydown', this.handleKeys);
+
+  bindKeys(): void {
+    document.addEventListener('keydown', this.handleKeys.bind(this));
+  }
+  unbindKeys(): void {
+    document.removeEventListener('keydown', this.handleKeys.bind(this));
   }
 }
-
-export default InputManager;
